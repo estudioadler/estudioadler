cb0c0d63b2fd82711d7e1aaa15b1a13f30ce8cca
Estudio Adler <adler.gabriel20@gmail.com>
2026-04-29 13:09:47 -0300

    feat: atualizar regras do robots.txt para permitir acesso a novos bots e melhorar SEO

--- app/robots.ts
+++ app/robots.ts
@@ -4,10 +4,28 @@ const siteUrl = "https://estudioadler.com.br";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GoogleOther",
+        allow: "/",
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
